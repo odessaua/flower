bdd80b64a6607ebcf3d0f2fc700d4a9024606489
Andrey Korol <andkorol@gmail.com>
2017-02-14 16:59:37 +0200

    Point 16: wfp new model. SQL in SQL.txt

--- protected/models/WfpOrder.php
+++ protected/models/WfpOrder.php
@@ -6,24 +6,24 @@
  * The followings are the available columns in table 'WfpOrder':
  * @property string $id
  * @property integer $order_id
- * @property string $order_reference
+ * @property string $orderReference
  * @property string $reason
- * @property integer $reason_code
+ * @property integer $reasonCode
  * @property double $amount
  * @property string $currency
- * @property string $created_date
- * @property string $processing_date
- * @property string $card_pan
- * @property string $card_type
- * @property string $bank_country
- * @property string $bank_name
- * @property string $transaction_status
- * @property string $auth_code
- * @property double $refund_amount
- * @property string $settlement_date
- * @property double $settlement_amount
+ * @property string $createdDate
+ * @property string $processingDate
+ * @property string $cardPan
+ * @property string $cardType
+ * @property string $issuerBankCountry
+ * @property string $issuerBankName
+ * @property string $transactionStatus
+ * @property string $authCode
+ * @property double $refundAmount
+ * @property string $settlementDate
+ * @property double $settlementAmount
  * @property double $fee
- * @property string $merchant_signature
+ * @property string $merchantSignature
  */
 class WfpOrder extends CActiveRecord
 {
@@ -43,16 +43,16 @@ class WfpOrder extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('order_id, reason_code', 'numerical', 'integerOnly'=>true),
-			array('amount, refund_amount, settlement_amount, fee', 'numerical'),
-			array('order_reference, card_type', 'length', 'max'=>20),
-			array('reason, card_pan, bank_country, bank_name, transaction_status, auth_code, merchant_signature', 'length', 'max'=>255),
+			array('order_id, reasonCode', 'numerical', 'integerOnly'=>true),
+			array('amount, refundAmount, settlementAmount, fee', 'numerical'),
+			array('orderReference, cardType', 'length', 'max'=>20),
+			array('reason, cardPan, issuerBankCountry, issuerBankName, transactionStatus, authCode, merchantSignature', 'length', 'max'=>255),
 			array('currency', 'length', 'max'=>10),
-			array('settlement_date', 'length', 'max'=>50),
-			array('created_date, processing_date', 'safe'),
+			array('settlementDate', 'length', 'max'=>50),
+			array('createdDate, processingDate', 'safe'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, order_id, order_reference, reason, reason_code, amount, currency, created_date, processing_date, card_pan, card_type, bank_country, bank_name, transaction_status, auth_code, refund_amount, settlement_date, settlement_amount, fee, merchant_signature', 'safe', 'on'=>'search'),
+			array('id, order_id, orderReference, reason, reasonCode, amount, currency, createdDate, processingDate, cardPan, cardType, issuerBankCountry, issuerBankName, transactionStatus, authCode, refundAmount, settlementDate, settlementAmount, fee, merchantSignature', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -75,24 +75,24 @@ class WfpOrder extends CActiveRecord
 		return array(
 			'id' => 'ID',
 			'order_id' => 'Order',
-			'order_reference' => 'Order Reference',
+			'orderReference' => 'Order Reference',
 			'reason' => 'Reason',
-			'reason_code' => 'Reason Code',
+			'reasonCode' => 'Reason Code',
 			'amount' => 'Amount',
 			'currency' => 'Currency',
-			'created_date' => 'Created Date',
-			'processing_date' => 'Processing Date',
-			'card_pan' => 'Card Pan',
-			'card_type' => 'Card Type',
-			'bank_country' => 'Bank Country',
-			'bank_name' => 'Bank Name',
-			'transaction_status' => 'Transaction Status',
-			'auth_code' => 'Auth Code',
-			'refund_amount' => 'Refund Amount',
-			'settlement_date' => 'Settlement Date',
-			'settlement_amount' => 'Settlement Amount',
+			'createdDate' => 'Created Date',
+			'processingDate' => 'Processing Date',
+			'cardPan' => 'Card Pan',
+			'cardType' => 'Card Type',
+			'issuerBankCountry' => 'Issuer Bank Country',
+			'issuerBankName' => 'Issuer Bank Name',
+			'transactionStatus' => 'Transaction Status',
+			'authCode' => 'Auth Code',
+			'refundAmount' => 'Refund Amount',
+			'settlementDate' => 'Settlement Date',
+			'settlementAmount' => 'Settlement Amount',
 			'fee' => 'Fee',
-			'merchant_signature' => 'Merchant Signature',
+			'merchantSignature' => 'Merchant Signature',
 		);
 	}
 
@@ -116,24 +116,24 @@ class WfpOrder extends CActiveRecord
 
 		$criteria->compare('id',$this->id,true);
 		$criteria->compare('order_id',$this->order_id);
-		$criteria->compare('order_reference',$this->order_reference,true);
+		$criteria->compare('orderReference',$this->orderReference,true);
 		$criteria->compare('reason',$this->reason,true);
-		$criteria->compare('reason_code',$this->reason_code);
+		$criteria->compare('reasonCode',$this->reasonCode);
 		$criteria->compare('amount',$this->amount);
 		$criteria->compare('currency',$this->currency,true);
-		$criteria->compare('created_date',$this->created_date,true);
-		$criteria->compare('processing_date',$this->processing_date,true);
-		$criteria->compare('card_pan',$this->card_pan,true);
-		$criteria->compare('card_type',$this->card_type,true);
-		$criteria->compare('bank_country',$this->bank_country,true);
-		$criteria->compare('bank_name',$this->bank_name,true);
-		$criteria->compare('transaction_status',$this->transaction_status,true);
-		$criteria->compare('auth_code',$this->auth_code,true);
-		$criteria->compare('refund_amount',$this->refund_amount);
-		$criteria->compare('settlement_date',$this->settlement_date,true);
-		$criteria->compare('settlement_amount',$this->settlement_amount);
+		$criteria->compare('createdDate',$this->createdDate,true);
+		$criteria->compare('processingDate',$this->processingDate,true);
+		$criteria->compare('cardPan',$this->cardPan,true);
+		$criteria->compare('cardType',$this->cardType,true);
+		$criteria->compare('issuerBankCountry',$this->issuerBankCountry,true);
+		$criteria->compare('issuerBankName',$this->issuerBankName,true);
+		$criteria->compare('transactionStatus',$this->transactionStatus,true);
+		$criteria->compare('authCode',$this->authCode,true);
+		$criteria->compare('refundAmount',$this->refundAmount);
+		$criteria->compare('settlementDate',$this->settlementDate,true);
+		$criteria->compare('settlementAmount',$this->settlementAmount);
 		$criteria->compare('fee',$this->fee);
-		$criteria->compare('merchant_signature',$this->merchant_signature,true);
+		$criteria->compare('merchantSignature',$this->merchantSignature,true);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
